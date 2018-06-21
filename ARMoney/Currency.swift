61f19f5b15979b44deb07781cd2355d4daff6b27
Morten Just Petersen <mortenjust@mortenjust-macbookpro.roam.corp.google.com>
2018-06-21 10:16:27 +0200

    a lot of stuff

---
+++ ARMoney/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  ARMoney
+//
+//  Created by Morten Just Petersen on 6/18/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import UIKit
+
+class Currency: NSObject {
+
+}
